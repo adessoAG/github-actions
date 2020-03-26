05bcc4e673f67c548d51f7851530250ab4a9806e
Cem Caylak <cem.caylak@adesso.de>
2020-03-26 19:50:50 +0100

    Git Init

---
+++ src/test/java/de/adesso/githubactionsexample/GithubActionsExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package de.adesso.githubactionsexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubActionsExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
